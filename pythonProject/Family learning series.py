051b5b5daf9fb319b15793d5fb52caf09456d59a
Katukam-Anuroop <anuroopkatukam@gmail.com>
2024-05-01 10:33:40 +0530

    Day 1

---
+++ pythonProject/Family learning series.py
@@ -0,0 +1,29 @@
+# print('hello anuroop, how are you ?')
+
+# 1. ask user to enter first number
+# 2. Choose the operator
+# 3. Ask user to enter second number
+# 4. click on equals to sign
+# 5. check the output
+
+
+# input("Choose the operator: ")
+
+f_n = int(input("Enter first number: "))
+s_n = int(input("Enter the second number: "))
+output = f_n + s_n
+print(f"first_number: {f_n} , second_number : {s_n}, addition_output = {output}")
+
+
+# f_n = input("Enter first number: ")
+# s_n = input("Enter the second number: ")
+# print(int(f_n) + int(s_n))
+
+# first_number: {f_n} , second_number : {s_n}, addition_output = {f_n + s_n}
+# (first_number + second_number) * (third_number + fourth_number)
+
+# f_n = 45
+# s_n = 78
+# print(f_n+s_n)
+
+# print(7+10)
